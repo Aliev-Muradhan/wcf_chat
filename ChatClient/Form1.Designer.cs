a386d3c7a519ff066cb91c447451f4fc6a31619b
Aliev-Muradhan <titaliev1p11@gmail.com>
2023-09-27 20:45:36 +0300

    Добавьте файлы проекта.

---
+++ ChatClient/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ChatClient
+{
+    partial class FormChat
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormChat));
+            this.btConnDicon = new System.Windows.Forms.Button();
+            this.lbChat = new System.Windows.Forms.ListBox();
+            this.tbUserName = new System.Windows.Forms.TextBox();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btConnDicon
+            // 
+            this.btConnDicon.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btConnDicon.Location = new System.Drawing.Point(33, 24);
+            this.btConnDicon.Name = "btConnDicon";
+            this.btConnDicon.Size = new System.Drawing.Size(139, 35);
+            this.btConnDicon.TabIndex = 0;
+            this.btConnDicon.Text = "Connect";
+            this.btConnDicon.UseVisualStyleBackColor = true;
+            this.btConnDicon.Click += new System.EventHandler(this.btConnDicon_Click);
+            // 
+            // lbChat
+            // 
+            this.lbChat.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbChat.FormattingEnabled = true;
+            this.lbChat.ItemHeight = 20;
+            this.lbChat.Location = new System.Drawing.Point(33, 65);
+            this.lbChat.Name = "lbChat";
+            this.lbChat.Size = new System.Drawing.Size(493, 184);
+            this.lbChat.TabIndex = 1;
+            // 
+            // tbUserName
+            // 
+            this.tbUserName.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbUserName.Location = new System.Drawing.Point(178, 24);
+            this.tbUserName.Multiline = true;
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.Size = new System.Drawing.Size(348, 35);
+            this.tbUserName.TabIndex = 2;
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbMessage.Location = new System.Drawing.Point(33, 255);
+            this.tbMessage.Multiline = true;
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(493, 42);
+            this.tbMessage.TabIndex = 3;
+            this.tbMessage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbMessage_KeyDown);
+            // 
+            // FormChat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(558, 324);
+            this.Controls.Add(this.tbMessage);
+            this.Controls.Add(this.tbUserName);
+            this.Controls.Add(this.lbChat);
+            this.Controls.Add(this.btConnDicon);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormChat";
+            this.Text = "Chat";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormChat_FormClosing);
+            this.Load += new System.EventHandler(this.FormChat_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btConnDicon;
+        private System.Windows.Forms.ListBox lbChat;
+        private System.Windows.Forms.TextBox tbUserName;
+        private System.Windows.Forms.TextBox tbMessage;
+    }
+}
+
---
+++ ChatClient/Form1.cs
@@ -0,0 +1,88 @@
+﻿using ChatClient.ServiceChat;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatClient
+{
+    public partial class FormChat : Form, IServiceChatCallback
+    {
+        bool isConnected = false;
+        ServiceChatClient client;
+        int ID;
+        public FormChat()
+        {
+            InitializeComponent();
+        }
+
+        void ConnectUser()
+        {
+            if (!isConnected)
+            {
+                client = new ServiceChatClient(new System.ServiceModel.InstanceContext(this));
+                ID = client.Connect(tbUserName.Text);
+                tbUserName.Enabled = false;
+                btConnDicon.Text = "Disconnect";
+                isConnected = true;
+            }
+        }
+
+        void DisconnectUser()
+        {
+            if (isConnected)
+            {
+                client.Disconnect(ID);
+                client = null;
+                tbUserName.Enabled = true;
+                btConnDicon.Text = "Connect";
+                isConnected = false;
+            }
+        }
+
+        private void btConnDicon_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                DisconnectUser();
+            }
+            else
+            {
+                ConnectUser();
+            }
+        }
+
+        public void MsgCallback(string msg)
+        {
+            lbChat.Items.Add(msg);
+            lbChat.SelectedIndex = lbChat.Items.Count - 1;
+        }
+
+        private void FormChat_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormChat_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DisconnectUser();
+        }
+
+        private void tbMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (client != null)
+                {
+                    client.SendMsg(tbMessage.Text, ID);
+                    tbMessage.Text = string.Empty;
+                }
+            }
+        }
+    }
+}
